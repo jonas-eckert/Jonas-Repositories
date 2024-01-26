472937d70a9ef223daa66a3a9bc7e5a88be72f57
Jonas Eckert <jeckert@masoncompaniesinc.com>
2024-01-26 12:07:05 -0600

    updates

--- services/event-risk-service/v1/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  verbose: true,
-  maxWorkers: '90%',
-  workerThreads: true
-};
